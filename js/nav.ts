0a4011b1b9bb2a26e05a4f4c6bb028fbab23d19c
Angelo Shema <geoangercola@gmail.com>
2019-12-15 02:19:00 +0200

    Remove support for floating nav

--- js/nav.ts
+++
@@ -1,8 +0,0 @@
-function registerSidebar(): void {
-	let sidebar: Element = document.querySelector("nav.sidebar");
-	sidebar.addEventListener("mouseover", 
-		function() {
-			setTimeout(function(e: Event) {e.target.toggleAttribute("hovered");}, 200)
-		}
-	);
-}
